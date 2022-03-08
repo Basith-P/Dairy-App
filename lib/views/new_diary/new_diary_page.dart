c4b962fbf23ea4c019b00598005c69b60ad0c0fc
Basith P <basithpofficial@gmail.com>
2022-03-08 11:00:00 +0530

    Add date and scrollable textfield

--- lib/views/new_diary/new_diary_page.dart
+++ lib/views/new_diary/new_diary_page.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:th_diary/config/themes/colors.dart';
 
 class NewDiaryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dateTime = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Diary'),
       ),
-      body: Center(
-        child: Text('New Diary'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(DateFormat('EEEE').format(dateTime)),
+            Text(DateFormat('dd.MM.yyyy').format(dateTime)),
+            const SizedBox(height: 16.0),
+            Expanded(
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: primaryColor,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
