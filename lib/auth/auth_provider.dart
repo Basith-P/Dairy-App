f3bf64e22d814108d2bd097b84df92ea6323362e
Basith P <basithpofficial@gmail.com>
2022-03-09 09:28:48 +0530

    Add auth provider

---
+++ lib/auth/auth_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class User {
+  final String name;
+  final String email;
+  final String password;
+
+  User(this.name, this.email, this.password);
+}
+
+class AuthProvider with ChangeNotifier {
+  List<User> _users = [];
+
+  int login(String email, String password) {
+    for (var user in _users) {
+      if (user.email == email && user.password == password) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  void signUp(String name, String email, String password) {
+    _users.add(User(name, email, password));
+    notifyListeners();
+  }
+}
